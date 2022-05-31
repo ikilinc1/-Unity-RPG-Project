1ba9d55607771cd8da1049335d9e75e583f38a1d
ikilinc1 <ikilinc1@binghamton.edu>
2022-05-31 13:49:32 -0400

    fixed sprinting animations

--- RPG-Project/Assets/MyScripts/PlayerMove.cs
+++ RPG-Project/Assets/MyScripts/PlayerMove.cs
@@ -6,18 +6,30 @@ using UnityEngine.AI;
 public class PlayerMove : MonoBehaviour
 {
     private NavMeshAgent nav;
+    private Animator anim;
     private Ray ray;
     private RaycastHit hit;
+
+    private float x;
+    private float z;
+    private float velocitySpeed;
     
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Calculate velocity
+        var velocity = nav.velocity;
+        x = velocity.x;
+        z = velocity.z;
+        velocitySpeed = x + z;
+        
         if (Input.GetMouseButtonDown(0))
         {
             if (Camera.main != null) ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -26,6 +38,16 @@ public class PlayerMove : MonoBehaviour
             {
                 nav.destination = hit.point;
             }
+        }
+
+        if (velocitySpeed != 0)
+        {
+            anim.SetBool("sprinting", true);
+        }
+
+        if (velocitySpeed == 0)
+        {
+            anim.SetBool("sprinting", false);
         }
     }
 }
