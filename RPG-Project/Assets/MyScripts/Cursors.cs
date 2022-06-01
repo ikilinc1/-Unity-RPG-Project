9ddeedbf6a28469080900a2b0600eb649181bd72
ikilinc1 <ikilinc1@binghamton.edu>
2022-06-01 12:50:53 -0400

    added cursor package and implemented new cursor

---
+++ RPG-Project/Assets/MyScripts/Cursors.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cursors : MonoBehaviour
+{
+    public GameObject cursorObject;
+    public Sprite cursorBasic;
+    public Sprite cursorHand;
+    public Image cursorImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cursorObject.transform.position = Input.mousePosition;
+
+        if (Input.GetMouseButton(1))
+        {
+            cursorImage.sprite = cursorHand;
+        }
+    }
+}
