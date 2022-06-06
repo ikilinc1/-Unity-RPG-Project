80417cc9dae64da86f7834791d25ace48360d107
ikilinc1 <ikilinc1@binghamton.edu>
2022-06-06 12:08:21 -0400

    small improvement to display box

--- RPG-Project/Assets/MyScripts/HintMessage.cs
+++ RPG-Project/Assets/MyScripts/HintMessage.cs
@@ -8,6 +8,8 @@ public class HintMessage : MonoBehaviour,IPointerEnterHandler, IPointerExitHandl
 {
     public GameObject hintBox;
     public Text message;
+    private bool displaying = true;
+    private bool overIcon = false;
 
     private Vector3 screenPoint;
     // Start is called before the first frame update
@@ -19,21 +21,38 @@ public class HintMessage : MonoBehaviour,IPointerEnterHandler, IPointerExitHandl
     // Update is called once per frame
     void Update()
     {
-        
+        if (overIcon)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                displaying = false;
+                hintBox.SetActive(false);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            displaying = true;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        hintBox.SetActive(true);
-        screenPoint.x = Input.mousePosition.x + 320;
-        screenPoint.y = Input.mousePosition.y + 150;
-        screenPoint.z = 1f;
-        hintBox.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
-        MessageDisplay();
+        overIcon = true;
+        if (displaying)
+        {
+            hintBox.SetActive(true);
+            screenPoint.x = Input.mousePosition.x + 320;
+            screenPoint.y = Input.mousePosition.y + 150;
+            screenPoint.z = 1f;
+            hintBox.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+            MessageDisplay();
+        }
+        
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        overIcon = false;
         hintBox.SetActive(false);
     }
 
