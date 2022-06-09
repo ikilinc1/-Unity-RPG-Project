c26a55e43fe8fc711dead2763643174d870a73ab
ikilinc1 <ikilinc1@binghamton.edu>
2022-06-09 08:44:29 -0400

    buy script cont.

--- RPG-Project/Assets/MyScripts/BuyScript.cs
+++ RPG-Project/Assets/MyScripts/BuyScript.cs
@@ -24,15 +24,69 @@ public class BuyScript : MonoBehaviour
         max = itemAmountText.Length;
         currencyText.text = InventoryItems.gold.ToString();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void CloseShop()
     {
         shopUI.SetActive(false);
+    }
+
+    public void BuyButton()
+    {
+        for (int i = 0; i < max; i++)
+        {
+            if (itemAmountText[i] == compare)
+            {
+                max = i;
+                if (amount[i] > 0)
+                {
+                    if (tavern)
+                    {
+                        UpdateTavernAmount();
+                    }
+
+                    if (InventoryItems.gold >= cost[i])
+                    {
+                        if (inventoryItems[i] == 0)
+                        {
+                            InventoryItems.newIcon = iconNum[i];
+                            InventoryItems.iconUpdate = true;
+                        }
+                        InventoryItems.gold -= cost[i];
+                        if (tavern)
+                        {
+                            setTavernAmount(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void UpdateTavernAmount()
+    {
+        inventoryItems[0] = InventoryItems.breads;
+        inventoryItems[1] = InventoryItems.cheese;
+        inventoryItems[2] = InventoryItems.meat;
+    }
+    
+    void setTavernAmount(int item)
+    {
+        if (item == 0)
+        {
+            InventoryItems.breads++;
+        }
+        if (item == 1)
+        {
+            InventoryItems.cheese++;
+        }
+        if (item == 2)
+        {
+            InventoryItems.meat++;
+        }
+
+        amount[item]--;
+        itemAmountText[item].text = amount[item].ToString();
+        currencyText.text = InventoryItems.gold.ToString();
+        max = itemAmountText.Length;
     }
 }
