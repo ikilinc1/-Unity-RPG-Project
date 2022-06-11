e9f6fde8059947fef9962599f6c15e6ecd1694f8
ikilinc1 <ikilinc1@binghamton.edu>
2022-06-11 06:18:23 -0400

    implemented unlocking magic and spell ui

---
+++ RPG-Project/Assets/MyScripts/BookCollect.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookCollect : MonoBehaviour
+{
+    public GameObject magicUI;
+    public GameObject spellsUI;
+
+    public bool magicBook = false;
+    public bool spellsbook = false;
+    
+    private bool magicCollected = false;
+    private bool spellsCollected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spellsUI.SetActive(false);
+        magicUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (magicBook)
+            {
+                if (!magicCollected)
+                {
+                    magicUI.SetActive(true);
+                    magicCollected = true;
+                }
+            }
+            
+            if (spellsbook)
+            {
+                if (!spellsCollected)
+                {
+                    spellsUI.SetActive(true);
+                    spellsCollected = true;
+                }
+            }
+        }
+    }
+}
