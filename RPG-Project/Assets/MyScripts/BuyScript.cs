b58f44d10316a6db38f5fa696e868584bdfb92ea
ikilinc1 <ikilinc1@binghamton.edu>
2022-06-11 05:31:54 -0400

    updated buy script for wizard shop

--- RPG-Project/Assets/MyScripts/BuyScript.cs
+++ RPG-Project/Assets/MyScripts/BuyScript.cs
@@ -46,7 +46,10 @@ public class BuyScript : MonoBehaviour
                         {
                             UpdateTavernAmount();
                         }
-
+                        else
+                        {
+                            UpdateWizardAmount();
+                        }
                         if (InventoryItems.gold >= cost[i])
                         {
                             if (inventoryItems[i] == 0)
@@ -58,6 +61,10 @@ public class BuyScript : MonoBehaviour
                             if (tavern)
                             {
                                 SetTavernAmount(i);
+                            }
+                            else
+                            {
+                                SetWizardAmount(i);
                             }
                         }
                     }
@@ -77,6 +84,17 @@ public class BuyScript : MonoBehaviour
         inventoryItems[0] = InventoryItems.breads;
         inventoryItems[1] = InventoryItems.cheese;
         inventoryItems[2] = InventoryItems.meat;
+    }
+    
+    void UpdateWizardAmount()
+    {
+        inventoryItems[0] = InventoryItems.redPotions;
+        inventoryItems[1] = InventoryItems.purplePotions;
+        inventoryItems[2] = InventoryItems.bluePotions;
+        inventoryItems[3] = InventoryItems.greenPotions;
+        inventoryItems[4] = InventoryItems.pinkEggs;
+        inventoryItems[5] = InventoryItems.roots;
+        inventoryItems[6] = InventoryItems.leafDews;
     }
     
     void SetTavernAmount(int item)
@@ -100,6 +118,43 @@ public class BuyScript : MonoBehaviour
         max = itemAmountText.Length;
     }
 
+    void SetWizardAmount(int item)
+    {
+        if (item == 0)
+        {
+            InventoryItems.redPotions++;
+        }
+        if (item == 1)
+        {
+            InventoryItems.purplePotions++;
+        }
+        if (item == 2)
+        {
+            InventoryItems.bluePotions++;
+        }
+        if (item == 3)
+        {
+            InventoryItems.greenPotions++;
+        }
+        if (item == 4)
+        {
+            InventoryItems.pinkEggs++;
+        }
+        if (item == 5)
+        {
+            InventoryItems.roots++;
+        }
+        if (item == 6)
+        {
+            InventoryItems.leafDews++;
+        }
+
+        amount[item]--;
+        itemAmountText[item].text = amount[item].ToString();
+        currencyText.text = InventoryItems.gold.ToString();
+        max = itemAmountText.Length;
+    }
+    
     public void Bread()
     {
         compare = itemAmountText[0];
@@ -115,10 +170,57 @@ public class BuyScript : MonoBehaviour
         compare = itemAmountText[2];
         Check(2);
     }
+    public void RedPotion()
+    {
+        compare = itemAmountText[0];
+        Check2(0);
+    }
+    public void PurplePotion()
+    {
+        compare = itemAmountText[1];
+        Check2(1);
+    }
+    public void BluePotion()
+    {
+        compare = itemAmountText[2];
+        Check2(2);
+    }
+    public void GreenPotion()
+    {
+        compare = itemAmountText[3];
+        Check2(3);
+    }
+    public void PinkEgg()
+    {
+        compare = itemAmountText[4];
+        Check2(4);
+    }
+    public void Root()
+    {
+        compare = itemAmountText[5];
+        Check2(5);
+    }
+    public void LeafDew()
+    {
+        compare = itemAmountText[6];
+        Check2(6);
+    }
 
     void Check(int b)
     {
         if (amount[b] > 0)
+        {
+            canClick = true;
+        }
+        else
+        {
+            canClick = false;
+        }
+    }
+    
+    void Check2(int c)
+    {
+        if (amount[c] > 0)
         {
             canClick = true;
         }
