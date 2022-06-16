82852b9719e48de20dc8d42ed412b86f597e6fe2
ikilinc1 <ikilinc1@binghamton.edu>
2022-06-16 15:04:54 -0400

    set up main camera audio script

---
+++ RPG-Project/Assets/MyScripts/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioPlayer;
+    public AudioClip mainLoop;
+    public AudioClip tavernLoop;
+    public AudioClip battleLoop;
+    public int musicState = 1;
+    [HideInInspector]
+    public bool canPlay = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPlay)
+        {
+            canPlay = false;
+            if (musicState == 1)
+            {
+                audioPlayer.clip = mainLoop;
+                audioPlayer.Play();
+            }
+            if (musicState == 2)
+            {
+                audioPlayer.clip = tavernLoop;
+                audioPlayer.Play();
+            }
+            if (musicState == 3)
+            {
+                audioPlayer.clip = battleLoop;
+                audioPlayer.Play();
+            }
+        }
+        
+    }
+}
