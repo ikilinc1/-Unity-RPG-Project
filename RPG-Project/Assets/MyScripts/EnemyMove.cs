30d08836ad41b5c74d57ef750125b807d2ed388d
ikilinc1 <ikilinc1@binghamton.edu>
2022-07-16 06:12:19 -0400

    enemy move script and first enemy animation controller

--- RPG-Project/Assets/MyScripts/EnemyMove.cs
+++ RPG-Project/Assets/MyScripts/EnemyMove.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
+    private NavMeshAgent nav;
+    private Animator anim;
+    
+    private float x;
+    private float z;
+    private float velocitySpeed;
+    
+    private AnimatorStateInfo enemyInfo;
+
+    public GameObject player;
+    private float distance;
+    private bool isAttacking = false;
+    public float attackRange = 2.0f;
+    public float runRange = 12.0f;
+    
     public GameObject thisEnemy;
     private bool outlineOn = false;
     
@@ -11,6 +27,8 @@ public class EnemyMove : MonoBehaviour
     void Start()
     {
         thisEnemy.GetComponent<Outline>().enabled = false;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,6 +49,56 @@ public class EnemyMove : MonoBehaviour
                 thisEnemy.GetComponent<Outline>().enabled = false;
                 outlineOn = false;
             }
+        }
+
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        // Calculate velocity
+        var velocity = nav.velocity;
+        x = velocity.x;
+        z = velocity.z;
+        velocitySpeed = x + z;
+
+        if (velocitySpeed == 0)
+        {
+            anim.SetBool("running", false);
+        }
+        else
+        {
+            anim.SetBool("running", true);
+            isAttacking = false;
+        }
+
+        enemyInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < attackRange || distance > runRange)
+        {
+            nav.isStopped = true;
+            if (distance < attackRange && enemyInfo.IsTag("nonAttack"))
+            {
+                if (!isAttacking)
+                {
+                    isAttacking = true;
+                    anim.SetTrigger("attack");
+                }
+            }
+
+            if (distance < attackRange && enemyInfo.IsTag("attack"))
+            {
+                if (isAttacking)
+                {
+                    isAttacking = false;
+                }
+            }
+        }
+        else
+        {
+            nav.isStopped = false;
+            nav.destination = player.transform.position;
         }
     }
 }
